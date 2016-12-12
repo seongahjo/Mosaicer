6e31b4733b3f7b40667530a71f1469fe29d8804c
seongahjo <seongside@gmail.com>
2016-12-12 17:36:30 +0900

    web image upload

--- web.py
+++ web.py
@@ -4,14 +4,16 @@ from __future__ import division
 from __future__ import print_function
 from flask import Flask, jsonify,request
 import compare
-
+import os
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER']='/root/mosaicer32/'
 @app.route('/',methods=['POST'])
 def api():
   results=[]
   for image in request.files.getlist('images'):
-    output=compare.convert(image)
+    path=os.path.join(app.config['UPLOAD_FOLDER'],image.filename)
+    image.save(path)
+    output=compare.convert(image.filename)
     precision=compare.evaluate(output)
     results.append({'precision':precision})
   return jsonify(results=results)
