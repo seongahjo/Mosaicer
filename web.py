ac8b8523d85ba4d54fbcc33051689108b3aa7549
seongahjo <hahaha@naver.com>
2016-12-30 08:58:21 +0900

    web finish

--- web.py
+++ web.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from flask import Flask, jsonify,request
-from flask.ext.api import status
 import compare
 #from convert import convert_image, convert_images
 import binary_convert as bc
@@ -26,18 +25,16 @@ def api():
 
 
 @app.route('/train')
-def train(data_dir,train_dir):
+def train():
     data_dir=request.args.get('data_dir')
     train_dir=request.args.get('train_dir')
 
-    data_dir=os.path.join('/tmp/',data_dir)
-
-    if not os.path.exists(data_dir)
-        return status.HTTP_400_BAD_REQUEST
+    #if not os.path.exists(data_dir)
+    #    return status.HTTP_400_BAD_REQUEST
 
     train_dir=makeDir(train_dir)
     train.train(data_dir=data_dir,train_dir=train_dir)
-    return status.HTTP_202_ACCEPTED
+    return True
 
 @app.route('/upload',methods=['POST'])
 def upload():
@@ -46,7 +43,7 @@ def upload():
     for image in request.files.getlist('images'):
         path=os.path.join(directory,image.filename)
         image.save(path)
-    return status.HTTP_202_ACCEPTED
+    return True
 
 
 #Convert Images to binary files
@@ -56,20 +53,18 @@ def convert():
     data_dir=request.args.get('data_dir')
     label = request.args.get('label')
 
-    image_dir=os.path.join('/tmp/',image_dir)
-    if not os.path.exists(image_dir)
-        return status.HTTP_400_BAD_REQUEST
+    #if not os.path.exists(image_dir)
+    #    return status.HTTP_400_BAD_REQUEST
 
     data_dir=makeDir(data_dir)
     bc.convert(image_dir=image_dir,data_dir=data_dir,label=label)
-    return status.HTTP_202_ACCEPTED
+    return True
 
 
 
 
 
-def makeDir(dir_name):
-    directory=os.path.join('/tmp/',dir_name)
+def makeDir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
