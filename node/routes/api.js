29ac0f9190cffc5ff8ba24c5e406a0fcebcf8e41
seongahjo <hahaha@naver.com>
2016-12-30 09:19:05 +0900

    bug fix

--- node/routes/api.js
+++ node/routes/api.js
@@ -1,4 +1,4 @@
-var express= require('exprss')
+var express= require('express')
 var router = express.Router()
 var path= require('path')
 var axios = require('axios')
@@ -8,10 +8,10 @@ router.get('/convert',function(req,res,next){
     var folder = req.query.folder
     var label = req.query.label
     var image_dir=path.join('/tmp/',id,'upload')
-    var data_dir path.join('/tmp/',id,'data')
+    var data_dir= path.join('/tmp/',id,'data')
     var data={
       'image_dir': image_dir,
-      'data_dir':,data_dir
+      'data_dir':data_dir,
       'label':label
     }
     axios.get('http://localhost:9999/convert',{
