d5101d6c48f6ab10a2f516638a8024bf1e82069a
seongahjo <seongside@gmail.com>
2017-05-14 13:54:24 +0900

    add threshold config

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 
-FLAGS = tf.app.flags.FLAGS
 
 # compare
 tf.app.flags.DEFINE_integer('num_examples',1,
@@ -23,25 +22,26 @@ tf.app.flags.DEFINE_integer('num_classes',10,
                            """Number of classes to run.""")
 
 # train
-tf.app.flags.DEFINE_integer('max_steps', 100,
+tf.app.flags.DEFINE_integer('max_steps', 200,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
-
+tf.app.flags.DEFINE_integer('threshold',0.47,
+                            """minimum value to identify image.""")
 
 # mosaic
 tf.app.flags.DEFINE_integer('mosaic_label', 1,
                             """label you want to digitize""")
 
 
-#directory
+# directory
 tf.app.flags.DEFINE_string('image_dir', 'image',
                            """Path to the image data directory.""")
 tf.app.flags.DEFINE_string('data_dir', 'data',
                            """Path to the binary data directory.""")
+tf.app.flags.DEFINE_string('video_dir', 'video',
+                           """Path to the video data directory.""")
 tf.app.flags.DEFINE_string('train_dir', '/tmp/seongah_train',
                            """Path to the train data directory.""")
 tf.app.flags.DEFINE_string('temp_dir', '/tmp/seongah_temp',
                            """Path to the temp data directory.""")
-tf.app.flags.DEFINE_string('video_dir', 'video',
-                           """Path to the video data directory.""")
