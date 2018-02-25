b82646f65eb970d35645c2e460dc17a1adcc88f7
seongahjo <seongside@naver.com>
2018-02-25 15:33:09 +0900

    airbnb js style

--- mosaicer/node/app.js
+++ mosaicer/node/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -8,8 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var util = require('./routes/util');
 var files = require('./routes/file');
-var socket= require('./routes/socket')
-var api = require('./routes/api')
+var socket= require('./routes/socket');
+var api = require('./routes/api');
 var app = express();
 
 // view engine setup
@@ -27,18 +27,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/util', util);
 app.use('/file',files);
-app.use('/socket',socket)
-app.use('/api',api)
+app.use('/socket',socket);
+app.use('/api',api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  'use strict';
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
+  'use strict';
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- mosaicer/node/util/file.js
+++ mosaicer/node/util/file.js
@@ -1,42 +1,47 @@
-var path=require('path')
-var fs=require('fs')
-var fse=require('fs-extra')
+var fs=require('fs');
+var fse=require('fs-extra');
 
 exports.make=(Path)=>{
+  'use strict';
   if(!fs.existsSync(Path)){
-  fs.mkdirSync(Path)
+  fs.mkdirSync(Path);
 }
 else {
 }
-}
+};
 
 exports.remove=(Path)=>{
+  'use strict';
   if (fs.existsSync(Path)) {
-    fse.removeSync(Path)
+    fse.removeSync(Path);
   }
-}
+};
 
 exports.move=(src,dest,option)=>{
+  'use strict';
   if (fs.existsSync(src)) {
-    fse.move(src, dest,option)
+    fse.move(src, dest,option);
   }
-}
+};
 
 exports.copy=(src,dest,option)=>{
+  'use strict';
   if (fs.existsSync(src)) {
-    fse.copySync(src, dest,option)
+    fse.copySync(src, dest,option);
   }
-}
+};
 
 
 exports.empty=(Path)=>{
+  'use strict';
     if (fs.existsSync(Path)) {
-      fse.emptyDir(Path)
+      fse.emptyDir(Path);
     }
-}
+};
 
-exports.readJson=(file)=>fse.readJsonSync(file,{throws: false})
+exports.readJson=(file)=>{'use strict'; fse.readJsonSync(file,{throws: false});};
 
 exports.writeJson=(file,content) =>{
-  fse.writeJsonSync(file,content)
-}
+  'use strict';
+  fse.writeJsonSync(file,content);
+};
