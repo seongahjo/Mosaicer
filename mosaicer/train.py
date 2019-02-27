f5c8147c6b3c4aa818c0019d5c0a174cba0747d4
seongahjo <seongside@gmail.com>
2019-02-28 03:30:59 +0900

    train: add cli options

--- mosaicer/train.py
+++ mosaicer/train.py
@@ -1,7 +1,8 @@
+import argparse
 import os
 from datetime import datetime
 import matplotlib.pyplot as plt
-from keras.applications import InceptionV3
+from keras.applications import InceptionV3, VGG16, MobileNet
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from keras.engine.saving import load_model
 from keras.layers import np, Dense, Dropout, GlobalAveragePooling2D
@@ -10,16 +11,18 @@ from keras.preprocessing import image
 from keras_applications.inception_v3 import preprocess_input
 from keras_preprocessing.image import ImageDataGenerator
 
+MODEL = "inceptionv3"
 MODEL_SAVE_FOLDER_PATH = './model/'
 model_path = MODEL_SAVE_FOLDER_PATH + 'test.h5'
 IMAGE_SIZE = (299, 299)
+IMAGE_PATH = './image'
+BATCH_SIZE = 10
+LOG_DIRECTORY_ROOT = 'log'
+EPOCHS = 5
+CLASSES = 2
 
 
 def visualize_data(positive_images, negative_images):
-    # INPUTS
-    # positive_images - Images where the label = 1 (True)
-    # negative_images - Images where the label = 0 (False)
-
     figure = plt.figure()
     count = 0
     for i in range(positive_images.shape[0]):
@@ -37,11 +40,7 @@ def visualize_data(positive_images, negative_images):
 
 
 def load_image():
-    IMAGE_PATH = './image'
-
-    BATCH_SIZE = 10
-
-    train_datagen = ImageDataGenerator(
+    train_data_generator = ImageDataGenerator(
         preprocessing_function=preprocess_input,
         rotation_range=40,
         width_shift_range=0.2,
@@ -51,7 +50,7 @@ def load_image():
         horizontal_flip=True,
         fill_mode='nearest')
 
-    validation_datagen = ImageDataGenerator(
+    validation_data_generator = ImageDataGenerator(
         preprocessing_function=preprocess_input,
         rotation_range=40,
         width_shift_range=0.2,
@@ -61,57 +60,39 @@ def load_image():
         horizontal_flip=True,
         fill_mode='nearest')
 
-    train_generator = train_datagen.flow_from_directory(IMAGE_PATH,
-                                                        target_size=IMAGE_SIZE,
-                                                        batch_size=BATCH_SIZE,
-                                                        class_mode='categorical')
-    validation_generator = validation_datagen.flow_from_directory(
+    train_generator = train_data_generator.flow_from_directory(IMAGE_PATH,
+                                                               target_size=IMAGE_SIZE,
+                                                               batch_size=BATCH_SIZE,
+                                                               class_mode='categorical')
+    validation_generator = validation_data_generator.flow_from_directory(
         IMAGE_PATH,
         target_size=IMAGE_SIZE,
         batch_size=BATCH_SIZE,
         class_mode='categorical')
-    # file_paths = glob.glob(path.join(IMAGE_PATH, '**', '*.jpg'))
-    # images = np.asarray([image.img_to_array(image.load_img(path, target_size=IMAGE_SIZE)) for path in file_paths])
-    # image_size = images.shape[0]
-    # read_labels = [path.split(os.path.sep)[-2] for path in file_paths]
-    # set_label = list(set(read_labels))
-    # labels = np.zeros((BATCH_SIZE,images.shape[0]))
-    # for i in range(image_size):
-    #     labels[i] = set_label.index(read_labels[i])
-    #
-    # TRAIN_TEST_SPLIT = 0.9
-    # split_index = int(TRAIN_TEST_SPLIT * image_size)
-    # shuffled_indices = np.random.permutation(image_size)
-    # train_indices = shuffled_indices[0:split_index]
-    # test_indices = shuffled_indices[split_index:]
-    #
-    # x_train = images[train_indices, :, :, :]
-    # y_train = labels[train_indices]
-    # x_test = images[test_indices, :, :, :]
-    # y_test = labels[test_indices]
 
-    # return [(x_train, y_train), (x_test, y_test)]
     return (train_generator, validation_generator)
 
 
 def make_model():
-    base_model = InceptionV3(include_top=False)
+    if MODEL == "inceptionv3":
+        base_model = InceptionV3(include_top=False, input_shape=IMAGE_SIZE + (3,))
+    elif MODEL == "vgg16":
+        base_model = VGG16(include_top=False, input_shape=IMAGE_SIZE + (3,))
+    elif MODEL == "mobilenet":
+        base_model = MobileNet(include_top=False, input_shape=IMAGE_SIZE + (3,))
+    else:
+        base_model = None
     x = base_model.output
     x = GlobalAveragePooling2D(name='avg_pool')(x)
     x = Dropout(0.4)(x)
-    last_layer = Dense(2, activation='softmax')(x)
+    last_layer = Dense(CLASSES, activation='softmax')(x)
     model = Model(inputs=base_model.input, outputs=last_layer)
-
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-
     model.summary()
     return model
 
 
 def train():
-    LOG_DIRECTORY_ROOT = 'log'
-    EPOCHS = 5
-
     now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     if not os.path.exists(MODEL_SAVE_FOLDER_PATH):
         os.mkdir(MODEL_SAVE_FOLDER_PATH)
@@ -120,11 +101,8 @@ def train():
     cb_checkpoint = ModelCheckpoint(filepath=model_path, monitor='val_loss', verbose=1, save_best_only=True)
     cb_early_stopping = EarlyStopping(monitor='val_loss', patience=1)
     tensorboard = TensorBoard(log_dir=log_dir, write_grads=True, write_images=True)
-
     model = make_model()
-
     generator = load_image()
-
     history = model.fit_generator(generator[0],
                                   validation_data=generator[1],
                                   epochs=EPOCHS,
@@ -160,22 +138,27 @@ def predict(model, img):
     return preds[0]
 
 
-def plot_preds(img, preds):
-    """Displays image and the top-n predicted probabilities in a bar graph
-    Args:
-        preds: list of predicted labels and their probabilities
-    """
-    plt.imshow(np.asarray(img))
-    plt.show();
-
-
 def test():
     model = load_model(model_path)
     img = image.load_img('image/jo/irene.jpg', target_size=IMAGE_SIZE)
     preds = predict(model, img)
     print(preds)
-    plot_preds(np.asarray(img), preds)
 
 
-# train()
-test()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", help="choose model", default="inceptionv3")
+    parser.add_argument("--width", help="image width size", default=299)
+    parser.add_argument("--height", help="image height size", default=299)
+    parser.add_argument("-b", "--batch", help="batch size", default=10)
+    parser.add_argument("-e", "--epochs", help="epochs", default=5)
+    parser.add_argument("-c", "--classes", help="the number of classes", default=2)
+    parser.add_argument("-i", "--image", help="image path", default="./image")
+    args = parser.parse_args()
+    IMAGE_SIZE = (args.width, args.height)
+    BATCH_SIZE = args.batch
+    EPOCHS = args.epochs
+    CLASSES = args.classes
+    IMAGE_PATH = args.image
+    MODEL = args.model
+    train()
