b5ee69809a6cd8433d99feea79bcb0e3898c38f5
seongahjo <seongside@gmail.com>
2019-09-27 09:24:15 +0900

    mosaic: OOP mosaic

--- mosaicer/mosaicer.py
+++ mosaicer/mosaicer.py
@@ -1,144 +1,135 @@
 import math
 import os
 import shutil
-import sys
 import time
+
 import cv2
 import face_recognition
-import numpy as np
-from keras.engine.saving import load_model
 
-from label import predict
+FPS = 30
+
+
+def frame_pre_processing(img):
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2RGB)
+    return img_output
 
 
 class Mosaicer(object):
     """ Do Digitize """
 
-    def __init__(self, model, batch_size):
-        self.images = []
-        self.positions = []
+    def __init__(self, batch_size):
         self.batch_size = batch_size
-        self.frames = []
-        self.model = model
+        self.image_size = (224, 224)
 
-    def batch_job(self, frames):
+    def _face_recognition(self, frames):
+        """ 얼굴 찾기
+
+        :param frames: 입력된 동영상 프레임들
+        :return:
+            faces: 얼굴 이미지 배열, (frame_size, face_count)
+            positions: 동영상 얼굴 위치 배열, (frame_size, face_count)
+        """
+
+        def fn_batch(now_frames, face_locations):
+            face_images = []
+            positions = []
+
+            for frame, faces in zip(now_frames, face_locations):
+                position = []
+                each_face = []
+                for (top, right, bottom, left) in faces:
+                    img_face = frame[top:bottom, left:right]
+                    img_output = frame_pre_processing(img_face)
+                    each_face.append(cv2.resize(
+                        img_output, self.image_size, interpolation=cv2.INTER_AREA))
+                    position.append((top, right, bottom, left))
+                face_images.append(each_face)  # faces
+                positions.append(position)
+            return face_images, positions
+
+        frame_size = len(frames)
+        epoch = math.ceil(frame_size / self.batch_size)
+
         batch_of_face_locations = face_recognition.batch_face_locations(frames, number_of_times_to_upsample=0,
                                                                         batch_size=self.batch_size)
-        for frame, faces in zip(frames, batch_of_face_locations):
-            position = []
-            image_array = []
-            for (top, right, bottom, left) in faces:
-                img_face = frame[top:bottom, left:right]
-                img_yuv = cv2.cvtColor(img_face, cv2.COLOR_BGR2YUV)
-                img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
-                img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2RGB)
-                image_array.append(cv2.resize(
-                    img_output, (299, 299), interpolation=cv2.INTER_AREA))
-                position.append((top, right, bottom, left))
-            self.images.append(image_array)  # faces
-            self.positions.append(position)
+        for i in range(epoch):
+            epoch_from = i * self.batch_size
+            epoch_to = (i + 1) * self.batch_size if i < epoch - 1 else frame_size
 
-    def capture(self, video_path, label):
-        file_name = os.path.split(video_path)[-1]
-        result_path = pre_capture(video_path)
+            faces, positions = fn_batch(frames[epoch_from:epoch_to], batch_of_face_locations[epoch_from: epoch_to])
+            print('epoch {} found {} faces in {} frames'.format(i, len(faces), epoch_to - epoch_from))
+
+        return faces, positions
+
+    def _write(self, frames, video_size, result_path='video/result.avi'):
+        """
+        모자이크 처리된 동영상 쓰기
+        :param frames: 모자이크 처리된 프레임
+        :param video_size: 비디오 크기
+        :param result_path: 모자이크 처리된 동영상이 저장될 위치
+        :return:
+        """
+        foc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(result_path, foc, FPS, video_size)
+        for frame in frames:
+            out.write(frame)
+        out.release()
+        print('video digitized : {} '.format(result_path))
+
+    def _digitize(self, frames, list_pos):
+        """
+        프레임 모자이크
+        :param frames: 동영상 프레임 배열
+        :param list_pos: 얼굴 위치 배열
+        :return:
+            digitize_frames: 모자이크 처리된 동영상 프레임
+        """
+
+        def fn_digitize(f, p):
+            (top, right, bottom, left) = p
+            blur = cv2.blur(f[top:bottom, left:right], (70, 70))
+            f[top:bottom, left:right] = blur
+            return f
+
+        digitize_frames = []
+        for frame, frame_pos in zip(frames, list_pos):
+            now_frame = frame
+            for pos in frame_pos:
+                now_frame = fn_digitize(now_frame, pos)
+            digitize_frames.append(now_frame)
+
+        return digitize_frames
+
+    def _capture(self, video_path):
+        """
+        동영상 읽기
+        :param video_path:
+        :return:
+            frames: 동영상의 프레임들
+            video_size: 비디오 크기
+        """
         cap = cv2.VideoCapture(video_path)
-        fps = 30.0
         width = int(cap.get(3))
         height = int(cap.get(4))
         start_time = time.time()
-        feedback = []
-        foc = cv2.VideoWriter_fourcc(*'XVID')
-        out = cv2.VideoWriter(result_path, foc, fps, (width, height))
-        temp = []
-
+        frames = []
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret or cv2.waitKey(1) & 0xFF == ord('q'):  # finished
                 break
-            temp.append(frame)
-            self.frames.append(frame)
+            frames.append(frame)
         cap.release()
-        print('frames : {0}'.format(len(self.frames)))
-
-        frame_size = len(self.frames)
-        for i in range(math.ceil(frame_size / self.batch_size)):
-            print(i)
-            if (i + 1) * self.batch_size > frame_size:
-                self.batch_job(frames=self.frames[i * self.batch_size: frame_size])
-            else:
-                self.batch_job(frames=self.frames[i * self.batch_size:(i + 1) * self.batch_size])
-
-        if label is not None:
-            chks, feedback = self.check_image(label=label)
-            # mosaic 과정
-            for chk, frame, position in zip(chks, self.frames, self.positions):
-                for flag, pos in zip(chk, position):
-                    if flag:
-                        (top, right, bottom, left) = pos
-                        frame = digitize(frame, top, right, bottom, left)
-                out.write(frame)
-
-        out.release()
+        print('frames : {0}'.format(len(frames)))
         end_time = time.time()
         print('time : {0}'.format(end_time - start_time))
+        return frames, (width, height)
 
-        count = 0
-        for image in feedback:
-            count += 1
-            name = file_name + '_face' + str(count)
-            file_path = os.path.join('feedback', name + '.jpg')
-            cv2.imwrite(file_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-
-    def check_image(self, label):
-        chks = []
-        precisions = predict(self.model, self.images)
-        feedback = []
-
-        for frame_image, frame_precision in zip(self.images, precisions):
-            chk = []
-            for face, precision in zip(frame_image, frame_precision):
-                print(precision)
-                # biggest = max(precision, key=(lambda key: precision[key]))
-                if precision[0] > 0.5:
-                    chk.append(False)
-                else:
-                    chk.append(True)
-
-            chks.append(chk)
-            norm = np.std(precision)
-            if 0 <= norm < 0.3:
-                print('feedback_add')
-                feedback.append(face)
-
-            # 자 이제 라벨을 어떻게할까
-            # 폴더 불러올 때 라벨 따로 불러오던가 해야함
-            # Feedback
-            # temp = []
-            # for data in precision.values():
-            #     temp.append(data)
-            # temp = np.array(temp)
-            # std = np.std(temp)
-            # if 0 <= std < 0.2:
-            #     if not compare_face(feedback, image):
-            #         feedback.append(image)
-        return chks, feedback
-
-
-def digitize(frame, top, right, bottom, left):
-    blur = cv2.blur(frame[top:bottom, left:right], (70, 70))
-    frame[top:bottom, left:right] = blur
-    return frame
-
-
-def pre_capture(video_path):
-    video_dir, filename = os.path.split(video_path)
-    feedback_dir = 'feedback'
-    result_dir = os.path.join(video_dir, 'result')
-    if not os.path.exists(result_dir):
-        os.makedirs(result_dir)
-    if not os.path.exists(feedback_dir):
-        os.makedirs(feedback_dir)
-    return os.path.join(result_dir, filename)
+    def run(self, video_path):
+        frames, video_size = self._capture(video_path)
+        faces, positions = self._face_recognition(frames)
+        self._write(self._digitize(frames, positions), video_size=video_size)
 
 
 def classify(src, des):
@@ -147,27 +138,6 @@ def classify(src, des):
     shutil.move(src, des)
 
 
-# Feedback
-# def compare_face(images, new_image):
-#     if not images:
-#         return False
-#     image_encodings = []
-#     for image in images:
-#         image_encoding = face_recognition.face_encodings(image)
-#         if image_encoding:
-#             image_encodings.append(image_encoding[0])
-#     unknown_encoding = face_recognition.face_encodings(new_image)
-#     if not unknown_encoding:
-#         return False
-#     unknown_encoding = unknown_encoding[0]
-#     face_distances = face_recognition.face_distance(
-#         image_encodings, unknown_encoding)
-#     return any(face_distance > 0.1 for face_distance in face_distances)
-
 if __name__ == "__main__":
-    mosaicer = Mosaicer(model=load_model('model/test2.h5'), batch_size=20)
-    if len(sys.argv) < 2:
-        print('[Error] ./%s [filename]' % sys.argv[0])
-    else:
-        path = os.path.join('video', sys.argv[1])
-        mosaicer.capture(video_path=path, label="test")
+    mosaicer = Mosaicer(batch_size=20)
+    mosaicer.run(video_path='video/test.avi')
